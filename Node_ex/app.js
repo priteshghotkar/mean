f4bf0b214337c9aae1aea0c4af51452dc7e8fb1b
PGhotkar <Pghotkar@slb.com>
2020-01-24 15:33:14 +0530

    undefined isue solved

--- Node_ex/app.js
+++ Node_ex/app.js
@@ -1,13 +1,14 @@
-const express = require('express')
+const express = require('express');
+const bodyParser =require('body-parser');
 const app = express();
-const mongoose=require('mongoose');
-const userrouter=require('./conntroller/userDashboard');
+app.use(bodyParser.json());
 const userdasrouter=require('./conntroller/usercontroller');
 const port = 3000;
+const mongoose=require('mongoose');
+//const userrouter=require('./conntroller/userDashboard');
 mongoose.connect('mongodb://localhost:27017/task-manager', {useNewUrlParser: true,useUnifiedTopology: true });
-
+//app.use(userdasrouter);
 app.use(userdasrouter);
-app.use(userrouter);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
